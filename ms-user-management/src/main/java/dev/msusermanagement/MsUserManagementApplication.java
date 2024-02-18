ac1af63cb9ffd6e7774dd67e7d3e1dda547c2c87
lfpo2005 <lfpo2005@yahoo.com.br>
2024-02-18 13:35:39 -0300

    add api gateway

---
+++ ms-user-management/src/main/java/dev/msusermanagement/MsUserManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.msusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserManagementApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(MsUserManagementApplication.class, args);
+    }
+
+}
